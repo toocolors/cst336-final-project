c0e4735ce4ca5147a7ad8f5c5a4b0f62071f6e55
toocolors <188366890+toocolors@users.noreply.github.com>
2025-11-29 14:15:58 -0800

    Update index.mjs
    Added functions to get user data.
    Added username_max constant.

--- index.mjs
+++ index.mjs
@@ -1,7 +1,11 @@
+// Imports
 import express from 'express';
 import mysql from 'mysql2/promise';
 import bcrypt from 'bcrypt';
 import session from 'express-session';
+
+// Global Constants
+const username_max = 8;
 
 // Setup app
 const app = express();
@@ -42,7 +46,16 @@ app.get('/', (req, res) => {
 
 // post login
 app.post('/login', (req, res) => {
-    // TODO: VaLidate login credentials
+    // Get username and password
+
+    // Check if username is empty or too long
+
+    // Check if password is empty
+
+    // Get user from database
+
+    // Execute SQL
+
 
     // Redirect to main page
     res.redirect('/');
@@ -80,6 +93,48 @@ app.listen(3000, () => {
 });
 
 // Functions
+/**
+ * Gets the information of a user from the database,
+ *  based on the passed in ID.
+ * Author: Noah deFer
+ * @param {Number} id The ID of a user.
+ * @returns The response from the SQL server.
+ */
+async function getUserById(id) {
+    // Build SQL Statement
+    let sql = `
+        SELECT *
+        FORM users
+        WHERE user_id = ?`;
+
+    // Execute SQL
+    const [rows] = await conn.query(sql, [id]);
+
+    // Return result
+    return rows;
+} // getUserById
+
+/**
+ * Gets the information of a user from the database,
+ *  based on the passed in username.
+ * Author: Noah deFer
+ * @param {String} username The username of a user.
+ * @returns The response from the SQL Server.
+ */
+async function getUserByUsername(username) {
+    // Build SQL Statement
+    let sql = `
+        SELECT *
+        FORM users
+        WHERE username LIKE ?`;
+
+    // Execute SQL
+    const [rows] = await conn.query(sql, [username]);
+
+    // Return result
+    return rows;
+} // getUserByUsername
+
 /**
  * Checks if the current session is authenticated.
  *  Redirects the user if it is not.
