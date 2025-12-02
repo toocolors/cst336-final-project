1097bb63107cb81e3db33788d27e00aebfd936da
toocolors <188366890+toocolors@users.noreply.github.com>
2025-12-02 13:16:57 -0800

    Update gameDetailsScript.js
    Added comments.

--- public/js/gameDetailsScript.js
+++ public/js/gameDetailsScript.js
@@ -2,6 +2,13 @@
 document.querySelector('#searchBtn').addEventListener('click', displayGameDetails);
 
 // Functions
+/**
+ * Takes a JSON object from RAWG API and
+ *  makes a JSON object with the values to be used on this page.
+ * Author: Noah deFer
+ * @param {JSON} data A JSON object representing a game.
+ * @returns The constructed, simplifyed JSON object.
+ */
 function buildGameObject(data) {
     // Build JSON with desired details
     let game = {
@@ -18,8 +25,14 @@ function buildGameObject(data) {
         'genres': data.genres
     };
     return game;
-}
+} // buildGameObject
 
+/**
+ * Gets game information from local storage or the API,
+ *  and updates page information using the game information.
+ * Author: Noah deFer
+ * @returns 
+ */
 async function displayGameDetails() {
     // Get Game Details
     let game = await getGameDetails();
@@ -83,8 +96,14 @@ async function displayGameDetails() {
         let genre = game.genres[i];
         genres.innerHTML += `${genre.name}<br>`
     }
-}
+} // displayGameDetails
 
+/**
+ * Looks for game information in local storage,
+ *  then from the API if no game was found.
+ * Author: Noah deFer
+ * @returns A JSON of game information (or false if the game was not found.)
+ */
 async function getGameDetails() {
     // Get game name
     let name = document.querySelector("#search").value.trim();
@@ -110,16 +129,28 @@ async function getGameDetails() {
 
     // Return Game Details
     return data;
-}
+} // getGameDetails
 
+/**
+ * Fetches game data from the website API.
+ * Author: Noah deFer
+ * @param {String} name The name of a game.
+ * @returns A JSON of game data (or false if game was not found.)
+ */
 async function getGameFromAPI(name) {
     console.log(`Getting ${name} from API...`);
     let response = await fetch(`/api/game/${name}`);
     data = await response.json();
 
     return data;
-}
+} // getGameFromAPI
 
+/**
+ * Attempts to get game data from local storage.
+ * Author: Noah deFer
+ * @param {String} name The name of a game.
+ * @returns A JSON of game data (or false if game was not found.)
+ */
 async function getGameFromStorage(name) {
     console.log(`Looking for ${name} in local storage...`);
     try {
@@ -136,9 +167,14 @@ async function getGameFromStorage(name) {
     }
 }
 
+/**
+ * Stores a JSON of game data in local storage.
+ * Author: Noah deFer
+ * @param {JSON} game A JSON of game data.
+ */
 function storeGame(game) {
     // Store Game Details in Local Storage
     console.log(`Storing ${game.name} into local storage...`);
     localStorage.setItem(`GameQuest/Game/${game.name}`, JSON.stringify(game));
     console.log(`Stored ${game.name} in local storage!`);
-}
+} // storeGame
