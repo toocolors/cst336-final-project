ad3b8776246c7c32fae826053bca2e6b4fdb80d7
toocolors <188366890+toocolors@users.noreply.github.com>
2025-12-03 06:38:35 -0800

    games page displays user collection.

---
+++ public/js/gamesScript.js
@@ -0,0 +1,31 @@
+// Add Event Listeners
+
+// Call functions
+updateCollection();
+
+// Functions
+async function getCollection() {
+    // Get user collection
+    let response = await fetch('/api/user-collection');
+    let data = await response.json();
+
+    // Return data
+    return data;
+}
+
+async function updateCollection() {
+    // Get user collection
+    let collection = await getCollection();
+    
+    // Update Collection container
+    let collectionContainer = document.querySelector('#collection');
+    for (let i = 0; i < collection.length; i++) {
+        // Get game
+        let game = collection[i];
+
+        // Add game to collection container
+        collectionContainer.innerHTML += `
+            <a href='/game/${game.game_id}'>${game.game_name}</a>
+            <br>`;
+    }
+}
